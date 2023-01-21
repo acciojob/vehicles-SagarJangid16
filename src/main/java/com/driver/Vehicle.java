013d3556a4fbf46cb6ad03b85ff339d776e8b2ef
Sagar <jangidsagar1614@gmail.com>
2023-01-21 19:10:56 +0530

    Solution done

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -30,15 +30,14 @@ public class Vehicle {
         System.out.println("stop method called - The vehicle is stopped");
     }
 
-    public String getName() {
-        return this.name;
-    }
-
     public int getCurrentSpeed() {
         return this.currentSpeed;
     }
 
     public int getCurrentDirection() {
         return this.currentDirection;
+    }
+    public String getName() {
+        return this.name;
     }
 }
